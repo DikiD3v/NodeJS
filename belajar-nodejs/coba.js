ce4672c9a6767da39a08451b84a37e319f0f4628
DikiD3v <dikisuhendi.dev@gmail.com>
2022-10-02 22:59:33 +0700

    belajar membuat file nodejs

---
+++ belajar-nodejs/coba.js
@@ -0,0 +1,5 @@
+console.log("Hello Word");
+
+const cetakNama = (nama) => `Hi, nama saya ${nama}`;
+
+module.exports = cetakNama;
---
+++ belajar-nodejs/index.js
@@ -0,0 +1,15 @@
+// console.log("Hello Diki");
+
+const cetakNama = require('./coba');
+
+// const cetakNama = (nama) => `Hi, nama saya ${nama}`;
+
+// console.log(cetakNama(nama));
+
+require('./coba');
+
+const nama = "diki suhendi";
+
+console.log(nama);
+
+console.log(cetakNama("Diki Suhendi"));
